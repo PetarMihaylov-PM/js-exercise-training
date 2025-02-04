08b0b6554d95b09335baab895b114f6795f35356
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-02-04 22:40:50 +0200

    Commit exercise

---
+++ Array Manipulator - array exercise/Array manipulator.js
@@ -0,0 +1,26 @@
+function solve(array, commands) {
+ 
+  let i = 0;
+  let command = commands[i];
+  i++;
+  
+  while (command !== 'print'){
+
+    let execute = command.split(' ')[0];
+
+    if (execute === 'add'){
+      let index = command.split(' ')[1];
+      let element = command.split(' ')[2];
+      array.splice(index, 0, element);
+    }
+    else if (execute === 'addMany'){
+      
+    }
+
+    command = commands[i].split(' ')[0];
+    i++;
+  } 
+}
+
+solve([1, 2, 4, 5, 6, 7],
+['add 1 8', 'contains 1', 'contains 3', 'print'])
