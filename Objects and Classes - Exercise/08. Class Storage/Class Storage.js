02ea3e48832f6d654362c3c0f68f924c284a43f0
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-02-23 20:35:42 +0200

    Upload exercise

---
+++ Objects and Classes - Exercise/08. Class Storage/Class Storage.js
@@ -0,0 +1,24 @@
+function solve () {
+  class Storage{
+    constructor(capacity){
+      this.capacity = capacity;
+      this.storage = [];
+      this.totalCost = 0;
+    }
+
+    addProduct(product) {
+      if(this.capacity >= product.quantity){
+        this.storage.push(product);
+        this.capacity -= product.quantity;
+        this.totalCost += product.price * product.quantity;
+      }
+      else {
+        console.log('Not enough capacity to add this product.');
+      }
+    }
+
+    getProducts() {
+      return this.storage.map(product => JSON.stringify(product)).join('\n');
+    }
+  }
+}
