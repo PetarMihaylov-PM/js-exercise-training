4f126cdd9d2fd06f16f1288566776ed4961c15d7
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-02-27 20:21:09 +0200

    Create Parking Lot.js

---
+++ Objects and Classes - Exercise/11. Parking Lot/Parking Lot.js
@@ -0,0 +1,36 @@
+function solve(array) {
+  let set = new Set();
+
+  array.forEach(element => {
+    let [direction, carNumber] = element.split(', ');
+
+    if(direction === 'IN'){
+      set.add(carNumber);
+    }
+    if(direction === 'OUT'){
+      set.delete(carNumber);
+    }
+  });
+
+  let sorted = Array.from(set);
+  sorted.sort((a, b) => {
+    let number1 = Number(a.split('').splice(2, 4).join(''));
+    let number2 = Number(b.split('').splice(2, 4).join(''));
+    console.log(number1,number2)
+    return number1 - number2;
+  });
+
+  if(set.size > 0){
+    sorted.forEach(car => {
+      console.log(car)
+    });
+  } else {
+    console.log('Parking Lot is Empty');
+  }
+}
+
+solve(['IN, CA2844AA',
+  'IN, CA1234TA',
+  'OUT, CA2844AA',
+  'OUT, CA1234TA']
+  )
