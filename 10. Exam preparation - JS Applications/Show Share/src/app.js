2eb15e80bccfae3c76453a78934287483df0948d
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-08-02 12:32:49 +0300

    Update app.js

---
+++ 10. Exam preparation - JS Applications/Show Share/src/app.js
@@ -0,0 +1,11 @@
+import { page } from './lib.js'
+import { addRender } from './utils/render.js'
+
+import { homeView } from './views/home.js'
+
+page(addRender);
+
+//bind project URLs to view handlers
+page('/', homeView);
+
+page.start();
