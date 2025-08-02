ee55ddb0044eae6f885de7065be9c0ca23cf3a30
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-08-02 22:49:48 +0300

    commit changes

--- 10. Exam preparation - JS Applications/Show Share/src/app.js
+++ 10. Exam preparation - JS Applications/Show Share/src/app.js
@@ -1,4 +1,5 @@
 import { page } from './lib.js'
+import { updateNav } from './utils/navBar.js';
 import { addRender } from './utils/render.js'
 
 import { homeView } from './views/home.js'
@@ -8,4 +9,5 @@ page(addRender);
 //bind project URLs to view handlers
 page('/', homeView);
 
+updateNav();
 page.start();
--- 10. Exam preparation - JS Applications/Show Share/src/lib.js
+++ 10. Exam preparation - JS Applications/Show Share/src/lib.js
@@ -1,8 +1,8 @@
-import page from "page";
-import { render, html, nothing } from "lit-html";
-import { classMap } from "lit-html/directives/class-map.js";
-import { styleMap } from "lit-html/directives/style-map.js";
-import { until } from "lit-html/directives/until.js";
+import  page  from "../node_modules/page/page.mjs";
+import { render, html, nothing } from "../node_modules/lit-html/lit-html.js";
+import { classMap } from "../node_modules/lit-html/directives/class-map.js";
+import { styleMap } from "../node_modules/lit-html/directives/style-map.js";
+import { until } from "../node_modules/lit-html/directives/until.js";
 
 
 
